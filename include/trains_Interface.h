f841f0a378ebb57ca5e11148e540a3fcfefad694
arroway <stephanie@minet.net>
2013-01-29 17:34:10 +0100

    update trains_Interface.h

--- include/trains_Interface.h
+++ include/trains_Interface.h
@@ -57,22 +57,6 @@ JNIEXPORT jint JNICALL Java_trains_Interface_utoBroadcast
 
 /*
  * Class:     trains_Interface
- * Method:    getMAX_MEMB
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_trains_Interface_getMAX_1MEMB
-  (JNIEnv *, jclass);
-
-/*
- * Class:     trains_Interface
- * Method:    getMyAddress
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_trains_Interface_getMyAddress
-  (JNIEnv *, jclass);
-
-/*
- * Class:     trains_Interface
  * Method:    initIDsMessageHeader
  * Signature: ()V
  */
